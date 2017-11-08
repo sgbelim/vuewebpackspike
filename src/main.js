df23690f3f947c951612c1a12f63adabc87900dc
salimgbelim <salimgbelim@gmail.com>
2017-11-08 15:50:45 +0000

    salim: vue filters example@

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.filter('capitalize', function(string){
+  var capitalFirst = string.charAt(0).toUpperCase();
+  var noCaseTail = string.slice(1, string.length);
+  console.log(noCaseTail);
+
+  return capitalFirst + noCaseTail; 
+});
+
+Vue.filter('capitalizeES6', function(string){
+  var [first, ...tail] = string;
+  return first.toUpperCase() + tail.join('')
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
